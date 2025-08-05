01a6d1501cee3a7e8c65328ecbc20a36ef0f5f74
Gatsdaporn Sayananon <yunastras@gmail.com>
2025-08-05 19:52:44 -0400

    added changes

--- main.js
+++ main.js
@@ -1,20 +1,60 @@
 
 
-let vocab = [];
 
 
-fetch('vocab.json')
-    .then(res => res.json())
-    .then(data => {
-    vocab = data;
+// fetch('vocab.json')
+//     .then(res => res.json())
+//     .then(data => {
+//     vocab = data;
+//     })
 
-    
+let vocab = null;
 
+// API / CALLING FILES / 
+async function loadVocab()   {
+    try {
+        const response = await fetch('vocab.json');
+        const data = await response.json();
+        vocab = data;
+        console.log(vocab);
 
-for (let i = 0; i <= 20; i++) {
-    console.log(vocab[i].pronoun, vocab[i].verb);
-
+    } catch (error) {
+        console.error('Error fetching JSON:', error);
+    }
 }
- console.log('Vocab data loaded:', vocab);
-    });
+
+loadVocab();
+
+// document.getElementById("showWordsBtn").onclick = 
+
+// document.getElementById("showWordsBtn").onclick = showFrenchWords;
+
+document.getElementById("userSubmit").onclick = function () {
+    const userInput = document.getElementById("userInput").value;
+    let correctness = "Incorrect";
+    for (let i = 0; i <= 20; i++) {
+        if (vocab[i] && userInput === vocab[i].answer) {
+            correctness = "Correct";
+        }
+    } }
+//     document.getElementById("correctness").textContent = `Answer is: ${correctness}`;
+// };
+
+    document.body.style.backgroundColor = "pink";
+
+//     let userAnswer;
+    
+// function showFrenchWords() {
+//     if (!vocab) return;
+//     document.getElementById("frenchWords").innerHTML = ""; 
+//     for (let i = 0; i <= 20; i++) {
+//         if (vocab[i]) {
+//             document.getElementById("frenchWords").innerHTML += `${vocab[i].pronoun} ${vocab[i].verb} <br>`;
+//         }
+//     }
+// }
+
+
    
+
+
