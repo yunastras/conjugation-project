d52c38957bbb342acd3ed175059d5b8843890d15
Gatsdaporn Sayananon <yunastras@gmail.com>
2025-08-05 21:38:38 -0400

    added changes

--- main.js
+++ main.js
@@ -16,45 +16,27 @@ async function loadVocab()   {
         const response = await fetch('vocab.json');
         const data = await response.json();
         vocab = data;
-        console.log(vocab);
-
+        return vocab;
     } catch (error) {
         console.error('Error fetching JSON:', error);
     }
+
 }
 
-loadVocab();
-
-// document.getElementById("showWordsBtn").onclick = 
-
-// document.getElementById("showWordsBtn").onclick = showFrenchWords;
-
-document.getElementById("userSubmit").onclick = function () {
-    const userInput = document.getElementById("userInput").value;
-    let correctness = "Incorrect";
-    for (let i = 0; i <= 20; i++) {
-        if (vocab[i] && userInput === vocab[i].answer) {
-            correctness = "Correct";
-        }
-    } }
-//     document.getElementById("correctness").textContent = `Answer is: ${correctness}`;
-// };
-
-    document.body.style.backgroundColor = "pink";
-
-//     let userAnswer;
-    
-// function showFrenchWords() {
-//     if (!vocab) return;
-//     document.getElementById("frenchWords").innerHTML = ""; 
-//     for (let i = 0; i <= 20; i++) {
-//         if (vocab[i]) {
-//             document.getElementById("frenchWords").innerHTML += `${vocab[i].pronoun} ${vocab[i].verb} <br>`;
-//         }
-//     }
-// }
 
 
-   
+function displayWords() {
+    let content = null
+    const data = loadVocab(); // be the array that is returned
+    console.log(data)
 
+    // get data from load vocab and loop through size of array
+    for(i = 0; i < 10; i++) {
 
+    }
+
+    document.getElementById("frenchWords").innerText = content
+
+}
+
+displayWords()
